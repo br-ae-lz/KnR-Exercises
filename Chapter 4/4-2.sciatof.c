9412a26acc1f382ffa5c58d702ae3d89f560845f
Braden <justaroundthere@gmail.com>
2020-12-18 17:34:16 -0500

    4-3 is (finally) complete!

--- Chapter 4/4-2.sciatof.c
+++ Chapter 4/4-2.sciatof.c
@@ -6,6 +6,7 @@
 
 #define MAXLINE 1000    // max char array length
 
+// Prompt is as follows:
 // Extend atof to handle scientific notation of the form 123.45e-6, where a 
 // floating-point number may be followed by e or E and an optionally signed exponent. 
 
