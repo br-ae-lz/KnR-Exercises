4b3fd35a686a3fd4d27be4bc9c6ff16c3a994653
Braden <justaroundthere@gmail.com>
2020-12-29 12:17:25 -0500

    4-14 complete!

---
+++ Chapter 4/4-14.swap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Prompt is as follows:
+// Define a macro swap(t,x,y) that interchanges two arguments of type t. 
+// (Block structure will help.)
+
+// swap(t,x,y): swaps two arguments of type t
+#define swap(t,x,y)\
+{\
+t temp = x;\
+x = y;\
+y = temp;\
+}
+
+void main()
+{
+    // tests swap when x and y are of type int; if successful, prints that "x = 2, y = 1"
+    int x = 1;
+    int y = 2;
+
+    swap(int, x, y);
+    printf("x = %d, y = %d\n", x, y);
+    
+    system("pause");
+}
