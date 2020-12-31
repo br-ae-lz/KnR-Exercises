b954be659f4141386255215b7f332d7f322c19c6
Braden <justaroundthere@gmail.com>
2020-12-31 11:26:41 -0500

    5-3 complete!

---
+++ Chapter 5/5-3.pstrcat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 1000
+
+// Prompt is as follows:
+// Write a pointer version of the function strcat that we showed in 
+// Chapter 2: strcat(s, t) copies the string t to the end of s.
+
+// Commented copy of original function:
+// /* strcat: concatenate t to end of s; s must be big enough */
+// void strcat(char s[], char t[])
+// {
+//     int i, j;
+//     i = j = 0;
+//     while (s[i] != '\0') /* find end of s */
+//         i++;
+//     while ((s[i++] = t[j++]) != '\0') /* copy t */
+//         ;
+// }
+
+void pstrcat(char *, char *);
+
+void main()
+{
+    // tests pstrcat; if successful, prints "oh my!\n" to the screen
+    char as[MAXLINE] = "oh ";
+    char at[MAXLINE] = "my!\n";
+    char *s = as;
+    char *t = at;
+
+    pstrcat(s, t);
+    printf(s);
+
+    system("pause");
+}
+
+// pstrcat: concatenate t to end of s (s must be big enough)
+void pstrcat(char *s, char *t)
+{
+    // find end of s
+    while (*s)
+        s++;
+    // copy t
+    while (*s++ = *t++)
+        ;
+}
